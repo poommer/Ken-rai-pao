30f9b004937a7fe1141591634395660139574028
pasayakorn <pasayakornj@gmail.com>
2024-08-26 08:37:31 +0700

    ?

--- user/homepage.php
+++ user/homepage.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../static/logo.png" type="image/x-icon">
-     <link rel="stylesheet" href="../style.css"> 
+     <link rel="stylesheet" type="text/css" href="../style.css"> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="../script.js"></script>
-    <title>Document</title>
+    <title>eiei</title>
 </head>
 <body>
     <div class="container">
-        
+        <div class="head-homepage"> 
+            <h1>eiei</h1>
+
+        </div>
     </div>
 </body>
 </html>
