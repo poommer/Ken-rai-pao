8fa0fa8ea00f3368ea33842779160c2db54df4a2
pasayakorn <pasayakornj@gmail.com>
2024-08-30 22:18:45 +0700

    homepage-finish,room_detail-not finished

--- user/homepage.php
+++ user/homepage.php
@@ -8,13 +8,41 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="../script.js"></script>
-    <title>eiei</title>
+    <title>homepage</title>
 </head>
 <body>
     <div class="container">
         <div class="head-homepage"> 
-            <h1>eiei</h1>
-
+            <div class="homepage-logo">
+                <img src="../static/logo.png" alt="logo" width="60px" height="60px">
+            </div>
+            <div class="homepage-text">
+                <h2>MCR BOOKING</h2>
+                <p>เอ็มซีอาร์ บุ๊กกิ้ง</p>
+            </div>
+            <div class="head-booking">
+                <img src="../static/calendar.png" alt="" width="25px" height="25px">
+                <a href>การจองของฉัน</a href>
+            </div>
+        </div>
+        <div class="">
+            รอภูมิทำ
+        </div>
+        <h2>BOOKING</h2>
+        <div class="homepage-booking">
+            <div class="homepage-room">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhW92Xms3PVXZwNiCuHAT4Gy7Pi510XmfzhQ&s" alt="">
+                <a href="">meeting MA2105 room</a>
+                <p>รายละเอียด</p>
+                <img src="../static/plus.png" alt="plus" width="20px"height="20px" class="plus">
+            </div>
+            
+            <div class="homepage-room">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhW92Xms3PVXZwNiCuHAT4Gy7Pi510XmfzhQ&s" alt="">
+                <a href="">meeting MA2105 room</a>
+                <p>รายละเอียด</p>
+                <img src="../static/plus.png" alt="plus" width="20px"height="20px" class="plus">
+            </div>
         </div>
     </div>
 </body>
--- user/room_detail.php
+++ user/room_detail.php
@@ -8,11 +8,30 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="../script.js"></script>
-    <title>Document</title>
+    <title>room_detail</title>
 </head>
 <body>
     <div class="container">
-        
+        <div class="head-room_detail">
+            <div class="btn-back">
+                <img src="https://cdn-icons-png.flaticon.com/128/507/507257.png" alt="" width="30px">
+                <a href="">BACK</a>
+            </div>
+            <h2 style=margin-top:10px>meeting ma2105 room</h2>
+        </div>
+        <div class="title-room_detail">
+            <img src="https://www.zemlar.ca/wp-content/uploads/2023/02/Essentials-of-Meeting-Rooms.jpg" alt="" width="450px" height="300px">
+            <h1 style=margin-left:10px;color:#722BC5;>meeting ma2105 room</h1>
+            <h3 style=margin-left:10px;>ห้องประชุม ma2105</h3>
+        </div>
+        <div class="">
+            <div class="">
+                <img src="" alt="">
+            </div>
+            <div class="">
+                
+            </div>
+        </div>
     </div>
 </body>
 </html>
