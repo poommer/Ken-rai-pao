2ffc59a877f5d930732ab9586dc11bcb6e5778d8
ASAMItheGOOOAT <65209010002@thonburi.ac.th>
2024-09-03 10:47:52 +0700

    Update booking_detail.php

--- user/booking_detail.php
+++ user/booking_detail.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="../script.js"></script>
-    <title>Document</title>
+    <title>bookingDetail</title>
 </head>
 <body>
     <div class="container bg-bookDetail">
